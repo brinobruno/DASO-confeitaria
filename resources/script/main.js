d5f5f1cd69a7b6b20fe48bc3e94728d37887b9c0
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-08-13 22:18:57 -0300

    content changes

--- resources/script/main.js
+++ resources/script/main.js
@@ -59,7 +59,8 @@ scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonials header, #testimonials .testimonials
+  #products .text, #products .menu-products,
+  #testimonials header, #testimonials .testimonials,
   #contact .text, #contact .links,
   footer .brand, footer .social
   `,
